774d108efe5cde8c1d5b8e0d4f2fffd7aa155337
Juned <jund7734@gmail.com>
2023-08-15 00:21:50 +0530

    navbar modified with the feature of current page indication

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -1,12 +1,23 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './Images/logo.png'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { GrFormClose } from "react-icons/gr";
 import Hamburger from './Hamburger';
 import Link from 'next/link';
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [currentPage, setcurrentPage] = useState('')
+
+
+  useEffect(() => {
+        let cpurl = window.location.href;
+        let cp= cpurl.split('/')[3];
+        console.log('useEffect :'+cp);
+        setcurrentPage(cp);
+        // console.log('current page :'+currentPage)
+  }, [])
+  
   return (
     <>
       <div className='border-b-2 bg-white p-4 sm:px-20 flex justify-between items-center relative'>
@@ -14,11 +25,11 @@ const Navbar = () => {
           <img src={logo.src} className="w-28" />
         </div>
         <div className='hidden sm:block'>
-          <span className='px-8 text-xl hover:scale-105 hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 relative   '><Link  href="/" className='hover:underline underline-offset-4'>Home</Link></span>
-          <span className='px-8 text-xl hover:scale-105 hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 relative   '><Link  href="/about" className='hover:underline underline-offset-4'>About</Link></span>
-          <span className='px-8 text-xl hover:scale-105 hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 relative   '><Link  href='/admission' className='hover:underline underline-offset-4'>Admission</Link></span>
-          <span className='px-8 text-xl hover:scale-105 hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 relative   '><Link  href='/parents' className='hover:underline underline-offset-4'>Parents</Link></span>
-          <span className='px-8 text-xl hover:scale-105 hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 relative   '><Link  href='/' className='hover:underline underline-offset-4'>More</Link></span>
+          <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage==''?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link  href="/" >Home</Link></span>
+          <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='about'?'font-semibold bg-red-800 rounded-md text-white':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link  href="/about" >About</Link></span>
+          <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='admission'?'font-semibold bg-red-800 rounded-md text-white':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link  href='/admission' >Admission</Link></span>
+          <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='parents'?'font-semibold bg-red-800 rounded-md text-white':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link  href='/parents' >Parents</Link></span>
+          <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='more'?'font-semibold bg-red-800 rounded-md text-white':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link  href='/' >More</Link></span>
         </div>
         <div className='text-3xl sm:hidden block ' >
          {showNavbar?<GrFormClose onClick={()=>setShowNavbar(false)} className={`  transition-all duration-200  `}  />:<GiHamburgerMenu onClick={()=>setShowNavbar(true)} className={` transition-all duration-200 `} /> }
@@ -26,11 +37,11 @@ const Navbar = () => {
         </div>
       </div>
       <div className={`flex flex-col justify-center overflow-hidden transition-all duration-300  ${showNavbar?'h-[250px]':'h-0'}`}>
-        <span className='px-8 m-2 text-xl  hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 '><Link href="/" className='hover:underline underline-offset-4'>Home</Link></span>
-        <span className='px-8 m-2 text-xl  hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 '><Link href="/about" className='hover:underline underline-offset-4'>About</Link></span>
-        <span className='px-8 m-2 text-xl  hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 '><Link href="/admission" className='hover:underline underline-offset-4'>Admission</Link></span>
-        <span className='px-8 m-2 text-xl  hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 '><Link href="/parents" className='hover:underline underline-offset-4'>Parents</Link></span>
-        <span className='px-8 m-2 text-xl  hover:text-red-800 hover:font-semibold  cursor-pointer transition-all duration-150 '><Link href="/" className='hover:underline underline-offset-4'>More</Link></span>
+        <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage==''?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link href="/" >Home</Link></span>
+        <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='about'?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link href="/about" >About</Link></span>
+        <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='admission'?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link href="/admission" >Admission</Link></span>
+        <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='parents'?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link href="/parents" >Parents</Link></span>
+        <span className={`px-3 mx-4 text-xl hover:scale-105  hover:font-semibold cursor-pointer transition-all duration-150 relative ${currentPage=='more'?'font-semibold bg-red-800 rounded-md text-white ':'hover:text-red-800 hover:underline underline-offset-4'}  `}><Link href="/" >More</Link></span>
       </div>
     </>
   )
