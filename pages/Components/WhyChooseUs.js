d0cd84a19f29dffb49724545ce8bffb1e9e65710
Juned <jund7734@gmail.com>
2023-08-15 13:38:40 +0530

    all good completed

--- pages/Components/WhyChooseUs.js
+++ pages/Components/WhyChooseUs.js
@@ -9,26 +9,26 @@ const WhyChooseUs = () => {
         <div className='my-24 px-4 md:px-28'>
             <h1 className=' text-4xl mt-8 md:text-5xl font-semibold text-center '>Why Choose Us?</h1>
             <div className='flex flex-col md:flex-row justify-center items-center md:py-12 my-8 md:my-20'>
-                <div className='border-2 border-black rounded-md p-5 w-[330px]  m-4 md:m-12 testimonial ' style={{border:'2px dashed black'}}>
+                <div className='border-2 border-black rounded-md p-5 w-[330px]  h-[480px] m-4 md:m-12 testimonial ' style={{border:'2px dashed black'}}>
                     <div className='p-3 flex justify-center'>
                         <img src={skilledTeacher.src} alt="" className='w-3/5' />
                     </div>
                     <h1 className='text-3xl font-bold text-center mb-4'>Skilled Teacher</h1>
-                    <p className='text-center text-xl font-semibold my-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='text-center text-xl font-semibold my-6'> We Have Skilled teachers possess in-depth knowledge and mastery of their subject matter, allowing them to provide accurate and comprehensive information.</p>
                 </div>
-                <div className='border-2 border-black rounded-md p-5 w-[330px] m-12 testimonial ' style={{border:'2px dashed black'}}>
+                <div className='border-2 border-black rounded-md p-5 w-[330px] h-[480px] m-12 testimonial ' style={{border:'2px dashed black'}}>
                     <div className='p-3 flex justify-center'>
                         <img src={money.src} alt="" className='w-3/5' />
                     </div>
                     <h1 className='text-3xl font-bold text-center mb-4'>Affordable Fees</h1>
-                    <p className='text-center text-xl font-semibold my-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='text-center text-xl font-semibold my-6'>We have Affordable fees that make education accessible to a wider range of individuals, regardless of their financial background.</p>
                 </div>
-                <div className='border-2 border-black rounded-md p-5 w-[330px] m-12 testimonial ' style={{border:'2px dashed black'}}>
+                <div className='border-2 border-black rounded-md p-5 w-[330px] h-[480px] m-12 testimonial ' style={{border:'2px dashed black'}}>
                     <div className='p-3 flex justify-center'>
                         <img src={modernLearning.src} alt="" className='w-3/5' />
                     </div>
                     <h1 className='text-3xl font-bold text-center mb-4'>Modern Learning</h1>
-                    <p className='text-center text-xl font-semibold my-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='text-center text-xl font-semibold my-6'> Modern learning embraces technology as a powerful tool for engagement, interactivity, and access to a wealth of information.</p>
                 </div>
             </div>
         </div>
