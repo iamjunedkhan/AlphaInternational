351b0761f6a39c8879d276e69d25d1453ab597bc
Juned <jund7734@gmail.com>
2023-08-22 21:29:12 +0530

    image issue resolved

--- pages/Components/TeacherCard.js
+++ pages/Components/TeacherCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import director from './Images/director.jpg'
+// import director from './Images/director.jpg'
 const TeacherCard = ({name,qualification,img}) => {
   return (
     <div className='  m-5 '>
-        <div className='w-64 h-64 overflow-hidden rounded-full mx-auto m-2'>
-            <img src={director.src} alt="" className='h-full w-full' />
+        <div className='w-64 h-64 overflow-hidden rounded-full mx-auto m-2 object-cover '>
+            <img src={img.src} alt="" className='h-full w-full object-cover' />
         </div>
         <h1 className='text-xl font-semibold text-center'>{name}</h1>
         <p className='text-sm text-gray-800 text-center' >{qualification}</p>
